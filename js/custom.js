8f435aad0b8a523b2370fdd7a4fdf8b450f49627
starahmad6161 <starahmad616161@gmail.com>
2020-11-28 00:22:15 +0200

    Update Quote page

--- js/custom.js
+++ js/custom.js
@@ -39,22 +39,25 @@ $(function () {
     /*Start Quote page */
     let dataFRow;
     let dataSRow;
+    let progressBar = 0;
 
+    
     //First Row
-    $(".quote-quote-section .first-row .box-item .icon").on('click', function() {
+    $(".quote-quote-section .first-row .box-item").on('click', function() {
         dataFRow = $(this).data('f-row');
-        
+        progressBar = 25;
+        changeProgressBar(0);
+        $('.quote-quote-section .tab-container').hide();
+
+
         $(`.quote-quote-section .second-row-box`).hide();
-        $(`.quote-quote-section .second-row-box_${dataFRow}`).show().siblings('.second-row-box').hide();
-
-
+        $(`.quote-quote-section .second-row-box_${dataFRow}`).fadeIn().siblings('.second-row-box').fadeOut();
 
         $(".quote-quote-section .second-row .box-item").removeClass('active');
 
         $('.quote-quote-section .inner-content .inner-box').hide();
-        $('.quote-quote-section .tab-container').hide();
 
-        $(this).parents('.box-item').addClass('active').siblings().removeClass('active');
+        $(this).addClass('active').siblings().removeClass('active');
         $('.quote-quote-section .second-row').addClass('active');
 
         $("html, body").animate({
@@ -63,15 +66,91 @@ $(function () {
     });
 
     //Second Row 
-    $(".quote-quote-section .second-row .box-item .icon").on('click', function() {
-        $(this).parents('.box-item').addClass('active').siblings().removeClass('active');
+    $(".quote-quote-section .second-row .box-item").on('click', function() {
         dataSRow = $(this).data('s-row');
-        $('.quote-quote-section .tab-container').show();
-        $(this).parents('.rows-container').find(`.content_${dataFRow}${dataSRow}`).show().siblings('.inner-box').hide();
+        //Reset all values
+        reset();
+        //bottom boxes `third row` after clicking 1st row and 2nd row 
+        let numArr = ['11','12','13'];
+        // if boxes id = 11 or 12 or 13 that mean it's include another required steps to increase progress bar width
+        if (numArr.includes(`${dataFRow}${dataSRow}`)) {
+            progressBar = 50;
+            changeProgressBar(0);
+        } else {
+            progressBar = 100;
+            changeProgressBar(0);
+        }
+
+        $(this).addClass('active').siblings().removeClass('active');
+        if (`${dataFRow}${dataSRow}` == "13") {
+            $('.quote-quote-section .tab-container').show();
+        } else {
+            $('.quote-quote-section .selling-tabs').show();
+        }
+        //$('.quote-quote-section .tab-container').show();
+        $(this).parents('.rows-container').find(`.content_${dataFRow}${dataSRow}`).fadeIn(200).siblings('.inner-box').fadeOut(200);
         
         $("html, body").animate({
             scrollTop: $(".quote-quote-section .inner-content").offset().top - 50
         }, 500);
     });
+
+    /**
+     * Add class 'selected' when click on buttons
+     */
+    $('.quote-quote-section .btn').on('click', function() {
+        $(this).addClass('selected').siblings().removeClass('selected red-btn');
+    });
+    
+
+
+    /**
+     * When Click buttons increase progress bar width
+     */
+    $(".fill-prog").on('click', function() {
+        let btnParent = $(this).parents('.btns');
+        let btnParentData = btnParent.data('progress');
+        if (!btnParent.hasClass('done')) {
+            changeProgressBar(btnParentData);
+            btnParent.addClass('done');
+        }
+    });
+    
+    /**
+     * When click out input to know if input is empty or has value
+     */
+    $('.input-fill-prog').on('blur', function() {
+        let dataFillProg = $(this).data('progress');
+        if ($(this).val() != '') {
+            if (!$(this).hasClass('done')) {
+                changeProgressBar(dataFillProg);
+                $(this).addClass('done');
+            }
+        } else {
+            if ($(this).hasClass('done')) {
+                changeProgressBar(-dataFillProg);
+                $(this).removeClass('done');
+            }
+        }
+    });
+
+    /**
+     * Change progress bar width 
+     */
+    function changeProgressBar(val) {
+        progressBar += val;
+        $(".quote-quote-progress .prog-bar").css('width', `${progressBar}%`);
+    }
+    
+    /**
+     * Reset all to re calc progress bar width
+     */
+    function reset() {
+        $("input").val('');
+        $(".fill-prog").parents('.btns').removeClass("done");
+        $(".input-fill-prog").removeClass("done");
+        $('.quote-quote-section .tab-container').hide();
+    }
+
     /*End Quote page */
 });
