34307870a52ec5a83dfd5f4c18869ff710c48d35
starahmad6161 <starahmad616161@gmail.com>
2020-11-28 02:46:58 +0200

    Update Quote page

--- js/custom.js
+++ js/custom.js
@@ -44,22 +44,31 @@ $(function () {
     
     //First Row
     $(".quote-quote-section .first-row .box-item").on('click', function() {
+        //get custom data value from html `data-f-row`
         dataFRow = $(this).data('f-row');
+        //make progress bar 25%
         progressBar = 25;
         changeProgressBar(0);
+
+        //Hide bottom tabs when click first row 
         $('.quote-quote-section .tab-container').hide();
-
-
+        //Show And hide second row
         $(`.quote-quote-section .second-row-box`).hide();
-        $(`.quote-quote-section .second-row-box_${dataFRow}`).fadeIn().siblings('.second-row-box').fadeOut();
-
+        let secondRowBox = $(`.quote-quote-section .second-row-box_${dataFRow}`);
+        secondRowBox.siblings('.second-row-box').fadeOut(200, function () {
+            secondRowBox.fadeIn(200)
+        });
+        //Remove class active from box-items [second row]
         $(".quote-quote-section .second-row .box-item").removeClass('active');
 
+        //hide all content below second row
         $('.quote-quote-section .inner-content .inner-box').hide();
-
+        
+        //add class active to selected box at 'first row'
         $(this).addClass('active').siblings().removeClass('active');
+        //add class active to second row to make it available to select
         $('.quote-quote-section .second-row').addClass('active');
-
+        //animate body to scroll to second row after selecting from first row (next step)
         $("html, body").animate({
             scrollTop: $(".quote-quote-section .second-row").offset().top - 50
         }, 500);
@@ -67,21 +76,29 @@ $(function () {
 
     //Second Row 
     $(".quote-quote-section .second-row .box-item").on('click', function() {
+        //get custom data value from html `data-s-row`
         dataSRow = $(this).data('s-row');
         //Reset all values
         reset();
-        //bottom boxes `third row` after clicking 1st row and 2nd row 
+        //bottom boxes `below second row` that contains more required steps like `price` 
         let numArr = ['11','12','13'];
-        // if boxes id = 11 or 12 or 13 that mean it's include another required steps to increase progress bar width
+        // if box id = 11 or 12 or 13 that mean it's include another required steps to increase progress bar width
         if (numArr.includes(`${dataFRow}${dataSRow}`)) {
+            //if this box required more steps data, then set progress bar 50%
             progressBar = 50;
             changeProgressBar(0);
         } else {
+            //if no more steps, then set progress bar 100%
             progressBar = 100;
             changeProgressBar(0);
         }
-
+        //add class active to selected box at 'second row'
         $(this).addClass('active').siblings().removeClass('active');
+        /**
+         * This trigger when select [1st row => 1st box] and [2nd row => 3rd item ] that mean box `13` will active
+         * if it is `box 13` [selling and buying home] that mean show => selling tabs and buying tabs at the bottom
+         * if not => that mean show selling tabs only
+         */
         if (`${dataFRow}${dataSRow}` == "13") {
             $('.quote-quote-section .tab-container').show();
         } else {
@@ -89,14 +106,14 @@ $(function () {
         }
         //$('.quote-quote-section .tab-container').show();
         $(this).parents('.rows-container').find(`.content_${dataFRow}${dataSRow}`).fadeIn(200).siblings('.inner-box').fadeOut(200);
-        
+        //animate body to scroll to (next step)
         $("html, body").animate({
             scrollTop: $(".quote-quote-section .inner-content").offset().top - 50
         }, 500);
     });
 
     /**
-     * Add class 'selected' when click on buttons
+     * Add class 'selected' when click at buttons
      */
     $('.quote-quote-section .btn').on('click', function() {
         $(this).addClass('selected').siblings().removeClass('selected red-btn');
@@ -143,7 +160,8 @@ $(function () {
     }
     
     /**
-     * Reset all to re calc progress bar width
+     * Reset all 
+     * to recalculate progress bar width
      */
     function reset() {
         $("input").val('');
@@ -152,6 +170,10 @@ $(function () {
         $('.quote-quote-section .tab-container').hide();
     }
 
+
+    /**
+     * When Scrolling set progress bar 'position fixed'
+     */
     let quoteProgress = $(".quote-quote-progress").offset().top;
     $(window).on('scroll', function () {
         if ($(window).scrollTop() >= quoteProgress) {
